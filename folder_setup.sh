faf836bf51dc0df8c35ba1363c1393a8991614cb
Eduardo José Barrios García <eduardojbarriosgarcia@gmail.com>
2023-10-29 17:59:36 +0000

    Adding setup files

---
+++ folder_setup.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Crear las carpetas principales
+mkdir -p factory_methods_java/classes
+mkdir -p factory_methods_java/factories
+mkdir -p factory_methods_java/gui
+mkdir -p factory_methods_java/interfaces
+
+echo "Directorios creados con éxito!"
+
