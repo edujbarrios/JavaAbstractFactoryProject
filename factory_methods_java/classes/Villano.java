c679659e8117f9f7de880fea611026e90885816c
Eduardo José Barrios García <eduardojbarriosgarcia@gmail.com>
2023-10-31 12:27:59 +0000

    Adding File Structure for "Villano.java"

---
+++ factory_methods_java/classes/Villano.java
@@ -0,0 +1,18 @@
+/**
+ * La clase Villano representa un villano en un juego o simulación.
+ * Implementa la interfaz Personaje, esta clase
+ * va a implementar todos los métodos definidos en esa interfaz "Personajes".
+ * En este caso concreto los personajes solo pueden atacar. 
+ */
+public class Villano implements Personaje {
+
+    /**
+     * Representa el nivel de maldad del villano.
+     */
+    private int nivelDeMaldad;
+
+    /**
+     * Método que representa la acción de atacar por parte del villano.
+     */
+    public void atacar() {}
+}
