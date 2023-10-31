32a57dddc292eff52f63f6a0e58091ebf9a908a0
Eduardo José Barrios García <eduardojbarriosgarcia@gmail.com>
2023-10-31 12:47:30 +0000

    Adding file structure for "Serpiente.java"

---
+++ factory_methods_java/classes/Serpiente.java
@@ -0,0 +1,18 @@
+/**
+ * La clase Serpiente representa una serpiente que sirve como mascota de un villano
+ * Implementa la interfaz Mascota, lo que sugiere que esta clas
+ */
+public class Serpiente implements Mascota {
+
+    /**
+     * Tipo de veneno que la serpiente posee.
+     * Caracteriza la peligrosidad de la serpiente diciendo el tipo de veneno que posee
+     * Así como sus capacidades en determinadas situaciones.
+     */
+    private String veneno;
+
+    /**
+     * Método que representa la acción de acompañar por parte de la serpiente al villano.
+     */
+    public void acompanar() {}
+}
