6f63b7355c4fa4401cc2a8b30384847f55d1b8d8
Eduardo José Barrios García <eduardojbarriosgarcia@gmail.com>
2023-10-31 17:47:54 +0000

    Adding Personaje interfaces

---
+++ factory_methods_java/interfaces/Personaje.java
@@ -0,0 +1,12 @@
+/**
+ * Interfaz Personaje que define el comportamiento que todos los personajes deben tener.
+ * Cualquier tipo específico de personaje que se cree debe implementar este contrato.
+ */
+public interface Personaje {
+
+    /**
+     * Método que simula la acción de un personaje atacando.
+     */
+    void atacar();
+
+}
