570746e3488a874b436313277fb0bc582eda893b
WhoKnows <alu0100918205@ull.edu.es>
2023-11-01 20:36:11 +0100

    Cambio de la ruta de la imagen de Grifo

--- src/classes/Grifo.java
+++ src/classes/Grifo.java
@@ -6,6 +6,6 @@ import javax.swing.*;
 public class Grifo implements Mascota {
     private double envergaduraAlas; 
     public ImageIcon acompanar() {
-        return new ImageIcon("./factory_methods_java/images/grifoimg.jpg");
+        return new ImageIcon("./images/grifoimg.jpg");
     }
 }
