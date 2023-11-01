d7331f264cd7d6c4ca955abee8a15ddda44e8b0c
WhoKnows <alu0100918205@ull.edu.es>
2023-11-01 20:10:39 +0100

    Actualizacion de la interfaz grafica, version con imagenes

--- src/gui/MainGUI.java
+++ src/gui/MainGUI.java
@@ -11,77 +11,85 @@ public class MainGUI extends JFrame {
     private JComboBox<String> factorySelector;
     private JCheckBox arma_checkbox, mascota_checkbox;
     private JButton finishButton;
-    private JLabel resultLabel, imgLabel;
+    private JLabel PersonajeLabel, ArmaLabel, MascotaLabel, ProblemaLabel;
 
     public MainGUI() {
         super("Fabrica de Personajes");
-        setSize(2000, 2000);
+        setSize(300, 110);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setLayout(new FlowLayout());
 
         // Selector de fábrica
         factorySelector = new JComboBox<>(new String[]{"Selecciona", "Héroes", "Villanos", "AntiHéroes"});
-        factorySelector.addItemListener(new ItemListener() {
-            public void itemStateChanged(ItemEvent e) {
-                if (e.getStateChange() == ItemEvent.SELECTED) {
-                    updateOptions((String) e.getItem());
-                }
-            }
-        });
         add(factorySelector);
 
         // CheckBoxes para armas
         arma_checkbox = new JCheckBox("Arma");
         add(arma_checkbox);
+        arma_checkbox.setVisible(true);
 
         // CheckBoxes para mascotas
         mascota_checkbox = new JCheckBox("Mascota");
         add(mascota_checkbox);
+        mascota_checkbox.setVisible(true);
 
         // Botón para terminar
         finishButton = new JButton("Terminar");
         finishButton.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent e) {
-                String factoryType = (String) factorySelector.getSelectedItem();
-                PersonajeFactory factory = new FabricaHeroes();
+                if (PersonajeLabel != null) {
+                    PersonajeLabel.setIcon(null);
+                }
+                if (ArmaLabel != null) {
+                    ArmaLabel.setIcon(null);
+                }
+                if (MascotaLabel != null) {
+                    MascotaLabel.setIcon(null);
+                }
 
-                if (factoryType.toLowerCase() == "heroe") {
+                String factoryType = (String) factorySelector.getSelectedItem();
+                PersonajeFactory factory = null;
+
+                if (factoryType == "Héroes") {
                     factory = new FabricaHeroes();
-                } else if (factoryType.toLowerCase() == "villano") {
+                } else if (factoryType == "Villanos") {
                     factory = new FabricaVillanos();
-                } else if (factoryType.toLowerCase() == "antiheroe") {
+                } else if (factoryType == "AntiHéroes") {
                     factory = new FabricaAntiHeroes();
                 }
 
-                Personaje personaje = factory.crearPersonaje();
-                Arma arma = factory.crearArma();
-                Mascota mascota = factory.crearMascota();
+                if (factory != null) {
+                    Personaje personaje = factory.crearPersonaje();
+                    Arma arma = factory.crearArma();
+                    Mascota mascota = factory.crearMascota();
 
-                ImageIcon icon = personaje.atacar();
-                imgLabel = new JLabel(icon);
-                add(imgLabel);
+                    ImageIcon PersonajeIcon = personaje.atacar();
+                    PersonajeLabel = new JLabel(PersonajeIcon);
+                    add(PersonajeLabel);
 
-                /**
-                 * Reajuste de ventana para mostrar correctamente las imagenes y el texto de la interfaz
-                */
-                pack();
-                setLocationRelativeTo(null);              
-                if (arma_checkbox.isSelected()) arma.usar();
-                if (mascota_checkbox.isSelected()) mascota.acompanar();
+                    if (arma_checkbox.isSelected()) {
+                        ImageIcon ArmaIcon = arma.usar();
+                        ArmaLabel = new JLabel(ArmaIcon);
+                        add(ArmaLabel);
+                    }
+
+                    if (mascota_checkbox.isSelected()) {
+                        ImageIcon MascotaIcon = mascota.acompanar();
+                        MascotaLabel = new JLabel(MascotaIcon);
+                        add(MascotaLabel);
+                    }
+
+                    /**
+                     * Reajuste de ventana para mostrar correctamente las imagenes y el texto de la interfaz
+                     */
+                    pack();
+                    setLocationRelativeTo(null);
+                }
             }
         });
         add(finishButton);
 
-        // Etiqueta para mostrar el resultado
-        resultLabel = new JLabel("Selecciona una fábrica y las opciones para crear tu personaje.");
-        add(resultLabel);
-
         setVisible(true);
-    }
-
-    private void updateOptions(String factoryType) {
-        arma_checkbox.setVisible(true);
-        mascota_checkbox.setVisible(true);
     }
 
     public static void main(String[] args) {
