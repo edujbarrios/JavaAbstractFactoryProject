c03423755cef0c9f6586190d5d265f6b7e086887
WhoKnows <alu0100918205@ull.edu.es>
2023-11-01 20:35:37 +0100

    Cambio de la ruta de la imagen de AntiHeroe

--- src/classes/AntiHeroe.java
+++ src/classes/AntiHeroe.java
@@ -6,6 +6,6 @@ import javax.swing.*;
 public class AntiHeroe implements Personaje {
     private int nivelDeConflicto;
     public ImageIcon atacar() {
-        return new ImageIcon("./factory_methods_java/images/antiheroeimg.jpg");
+        return new ImageIcon("./images/antiheroeimg.jpg");
     }
 }
