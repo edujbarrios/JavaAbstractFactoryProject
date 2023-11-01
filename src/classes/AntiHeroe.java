7ea27bfe484f6c43c3e7dbc03f07c143318527c7
WhoKnows <alu0100918205@ull.edu.es>
2023-11-01 19:35:20 +0100

    Cambios en la clase AntiHeroe para representar las imagenes

--- src/classes/AntiHeroe.java
+++ src/classes/AntiHeroe.java
@@ -1,8 +1,11 @@
 package classes;
 
 import interfaces.Personaje;
+import javax.swing.*;
 
 public class AntiHeroe implements Personaje {
-    private int nivelDeConflicto; 
-    public void atacar() {} 
+    private int nivelDeConflicto;
+    public ImageIcon atacar() {
+        return new ImageIcon("./factory_methods_java/images/antiheroeimg.jpg");
+    }
 }
