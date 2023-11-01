c688d0b1b56b9fbd13e51325127efd2ccd4d4ab9
WhoKnows <alu0100918205@ull.edu.es>
2023-11-01 20:34:20 +0100

    Cambio de la ruta de imagen Ballesta

--- src/classes/Ballesta.java
+++ src/classes/Ballesta.java
@@ -6,6 +6,6 @@ import javax.swing.*;
 public class Ballesta implements Arma {
     private double alcance;
     public ImageIcon usar() {
-        return new ImageIcon("./factory_methods_java/images/ballestaimg.jpg");
+        return new ImageIcon("./images/ballestaimg.jpg");
     }
 }
