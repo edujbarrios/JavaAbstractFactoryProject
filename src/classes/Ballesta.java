00c8df53b55fdd088ccd007b5a99ac12770a48f2
WhoKnows <alu0100918205@ull.edu.es>
2023-11-01 19:38:26 +0100

    Cambios en la clase Ballesta para mostrar la imagen

--- src/classes/Ballesta.java
+++ src/classes/Ballesta.java
@@ -1,8 +1,11 @@
 package classes;
 
 import interfaces.Arma;
+import javax.swing.*;
 
 public class Ballesta implements Arma {
-    private double alcance; 
-    public void usar() {} 
+    private double alcance;
+    public ImageIcon usar() {
+        return new ImageIcon("./factory_methods_java/images/ballestaimg.jpg");
+    }
 }
