129a4001237cc90ee6837c4e5d02d2123f643ba8
Juan Diego Rendón Cachafeiro <72521316+JuanDiego34@users.noreply.github.com>
2023-11-01 18:36:50 +0000

    Muestra de imagenes Dragon.java

--- src/classes/Dragon.java
+++ src/classes/Dragon.java
@@ -2,7 +2,11 @@ package classes;
 
 import interfaces.Mascota;
 
+import javax.swing.*;
+
 public class Dragon implements Mascota {
     private String colorDeFuego; 
-    public void acompanar() {} 
+    public ImageIcon acompanar() {
+        return new ImageIcon("./factory_methods_java/images/dragonimg.jpg");
+    }
 }
