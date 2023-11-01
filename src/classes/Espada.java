6a1a34361438d68e9a5f51297abfc472c5e9d094
Juan Diego Rendón Cachafeiro <72521316+JuanDiego34@users.noreply.github.com>
2023-11-01 19:33:45 +0000

    Update Espada.java

--- src/classes/Espada.java
+++ src/classes/Espada.java
@@ -7,6 +7,6 @@ import javax.swing.*;
 public class Espada implements Arma {
     private double peso;
     public ImageIcon usar() {
-        return new ImageIcon("./factory_methods_java/images/espadaimg.jpg");
+        return new ImageIcon("./images/espadaimg.jpg");
     }
 }
